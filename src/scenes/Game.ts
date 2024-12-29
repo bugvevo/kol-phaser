4b841cee3ea57ee6c12b1e2b016d73bcc995f336
Isabel Skidmore <isabelskidmore@gmail.com>
2024-12-28 20:48:24 -0600

    add desktop icons

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,27 +1,34 @@
 import { Scene } from 'phaser';
 import UIHelpers from '../helpers/UIHelpers';
 
-export class Game extends Scene
-{
+export class Game extends Scene {
+	constructor() {
+		super('Game');
+	}
 
-    constructor ()
-    {
-        super('Game');
-    }
+	create() {
+		this.addBrowserWindow();
+		this.addDesktopIcons();
+		UIHelpers.addCenteredText(this, 30, 'hello world');
 
-    create ()
-    {
-        this.addBrowserWindow();
-        UIHelpers.addCenteredText(this, 30, 'hello world');
+		this.input.once('pointerdown', () => {
+			this.scene.start('GameOver');
+		});
+	}
 
-        this.input.once('pointerdown', () => {
+	private addDesktopIcons() {
+		const startingY = 60;
+		const icons = ['iconKernia', 'iconDoomscrolls', 'readme'];
 
-            this.scene.start('GameOver');
+		icons.forEach((iconTexture, index) => {
+			this.add
+				.image(30, startingY + index * 82, 'textures', iconTexture)
+				.setOrigin(0, 0)
+				.setScale(2);
+		});
+	}
 
-        });
-    }
-
-    private addBrowserWindow() {
-        UIHelpers.addWindow(this, 194, 60, 800, 600);
-    }
+	private addBrowserWindow() {
+		UIHelpers.addWindow(this, 194, 60, 800, 600);
+	}
 }
