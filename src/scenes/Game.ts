a1a167772d10ee224edccb3fbd5cbb6498a772d8
Isabel Skidmore <isabelskidmore@gmail.com>
2024-12-28 20:03:14 -0600

    tear out some unused stuff. create the browser window

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,10 +1,8 @@
 import { Scene } from 'phaser';
+import UIHelpers from '../helpers/UIHelpers';
 
 export class Game extends Scene
 {
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
 
     constructor ()
     {
@@ -13,23 +11,17 @@ export class Game extends Scene
 
     create ()
     {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
-
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+        this.addBrowserWindow();
+        UIHelpers.addCenteredText(this, 30, 'hello world');
 
         this.input.once('pointerdown', () => {
 
             this.scene.start('GameOver');
 
         });
+    }
+
+    private addBrowserWindow() {
+        UIHelpers.addWindow(this, 194, 60, 800, 600);
     }
 }
