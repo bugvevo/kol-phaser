221a29df38ab1b934b2eb8041ee043e093b43a61
Isabel Skidmore <isabelskidmore@gmail.com>
2024-12-28 20:48:16 -0600

    mess with size and scaling of the game

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,28 @@ import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
-import { Game, Types } from "phaser";
+import { Game, Types } from 'phaser';
 
 const config: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+	type: Phaser.AUTO,
+	width: 960,
+	height: 720,
+	zoom: 1,
+	parent: 'game-container',
+	backgroundColor: '#028af8',
+	pixelArt: true,
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		min: { width: 640, height: 480 },
+	},
+	callbacks: {
+		postBoot: function (game) {
+			game.scale.displaySize.setSnap(320, 240);
+			game.scale.refresh();
+		},
+	},
+	scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 };
 
 export default new Game(config);
