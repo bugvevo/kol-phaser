5f81a50d6ca32c2960dbb6e601a5e218d5c4fa74
Isabel Skidmore <isabelskidmore@gmail.com>
2024-12-28 18:20:26 -0600

    fix up some formatting

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { Boot } from './scenes/Boot'
-import { GameOver } from './scenes/GameOver'
-import { StartUp } from './scenes/StartUp'
-import { Preloader } from './scenes/Preloader'
+import { Boot } from './scenes/Boot';
+import { GameOver } from './scenes/GameOver';
+import { StartUp } from './scenes/StartUp';
+import { Preloader } from './scenes/Preloader';
 
-import { Game, Types } from 'phaser'
+import { Game, Types } from 'phaser';
 
-//  Find out more information about the Game Config at:
-//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 1280,
@@ -18,6 +16,6 @@ const config: Types.Core.GameConfig = {
 		autoCenter: Phaser.Scale.CENTER_BOTH,
 	},
 	scene: [Boot, Preloader, StartUp, GameOver],
-}
+};
 
-export default new Game(config)
+export default new Game(config);
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,20 +1,20 @@
-import { Scene } from 'phaser'
+import { Scene } from 'phaser';
 
 export class Game extends Scene {
-	camera: Phaser.Cameras.Scene2D.Camera
-	background: Phaser.GameObjects.Image
-	msg_text: Phaser.GameObjects.Text
+	camera: Phaser.Cameras.Scene2D.Camera;
+	background: Phaser.GameObjects.Image;
+	msg_text: Phaser.GameObjects.Text;
 
 	constructor() {
-		super('Game')
+		super('Game');
 	}
 
 	create() {
-		this.camera = this.cameras.main
-		this.camera.setBackgroundColor(0x00ff00)
+		this.camera = this.cameras.main;
+		this.camera.setBackgroundColor(0x00ff00);
 
-		this.background = this.add.image(512, 384, 'background')
-		this.background.setAlpha(0.5)
+		this.background = this.add.image(512, 384, 'background');
+		this.background.setAlpha(0.5);
 
 		this.msg_text = this.add.text(
 			512,
@@ -28,11 +28,11 @@ export class Game extends Scene {
 				strokeThickness: 8,
 				align: 'center',
 			}
-		)
-		this.msg_text.setOrigin(0.5)
+		);
+		this.msg_text.setOrigin(0.5);
 
 		this.input.once('pointerdown', () => {
-			this.scene.start('GameOver')
-		})
+			this.scene.start('GameOver');
+		});
 	}
 }
